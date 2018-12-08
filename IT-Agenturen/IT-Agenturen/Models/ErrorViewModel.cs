a97e785a1d70f51f620562757ae0141f2276eb20
Allan Dahl <larrevick@hotmail.com>
2018-12-09 00:31:47 +0100

    Init

---
+++ IT-Agenturen/IT-Agenturen/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IT_Agenturen.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
